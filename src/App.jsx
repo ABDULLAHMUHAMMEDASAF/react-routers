fca2dfcf927546289021c97f0403144e5d0f002b
ABDULLAHMUHAMMEDASAF <emrecanemre49@gmail.com>
2026-05-13 10:32:04 +0300

    The project was cleaned for a new tutorial from gemini on 13.05.2026 at 10.32

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import Navigation from "./Navigation.jsx";
-import { Route, Routes } from "react-router";
-import Home from "./Home.jsx";
-import Users from "./Users.jsx";
-import NoMatch from "./Nomatch.jsx";
-import UserDetail from "./UserDetail.jsx";
 import "./App.css";
 
 function App() {
+  
   const users = [
     { id: 1, name: "Abullah Muhammed Asaf", age: 40 },
     { id: 2, name: "Zübeyir Bin Avvam", age: 50 },
@@ -18,16 +13,7 @@ function App() {
   return (
     <>
       <h1>React Routers</h1>
-      <div className="container">
-        <Navigation />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="kullanicilar" element={<Users users={users} />}>
-            <Route path=":userId" element={<UserDetail users={users} />} />
-          </Route>
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </div>
+      <div className="container"></div>
     </>
   );
 }
